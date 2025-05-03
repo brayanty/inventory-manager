3aeb57b18b848521b3e8a8ed95da1fd3cb69cd36
brayan <danielsegoviad@gmail.com>
2025-05-03 16:18:53 -0500

    change name

---
+++ frontend/src/components/constants/models.ts
@@ -0,0 +1,61 @@
+interface Models {
+  id: number;
+  nombre: string;
+  descripcion: string;
+}
+
+const models: Models[] = [
+  { id: 1, nombre: "Apple", descripcion: "Fabricante de dispositivos iOS" },
+  {
+    id: 2,
+    nombre: "Samsung",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 3,
+    nombre: "Huawei",
+    descripcion: "Fabricante de dispositivos Android y HarmonyOS",
+  },
+  {
+    id: 4,
+    nombre: "Xiaomi",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 5,
+    nombre: "Google",
+    descripcion: "Fabricante de dispositivos Android Pixel",
+  },
+  {
+    id: 6,
+    nombre: "OnePlus",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 7,
+    nombre: "Oppo",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 8,
+    nombre: "Vivo",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 9,
+    nombre: "Motorola",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 10,
+    nombre: "Nokia",
+    descripcion: "Fabricante de dispositivos Android",
+  },
+  {
+    id: 11,
+    nombre: "Otros",
+    descripcion: "Otras marcas mejor conocido como chinos",
+  },
+];
+
+export default models;
