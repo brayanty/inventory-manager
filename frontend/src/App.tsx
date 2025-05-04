6aa55239226e9a97d3a7c7a6f11fbc81a0d859dd
brayan <danielsegoviad@gmail.com>
2025-05-04 07:20:57 -0500

    add toasting container to main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router";
 import "./App.css";
 import AppRoutes from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer position="top-center" autoClose={3000} theme="dark" />
       <AppRoutes />
     </BrowserRouter>
   );
