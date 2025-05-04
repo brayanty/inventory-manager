d065a55760568f9571f6c5ada6c31dbba6fd7f5a
brayan <danielsegoviad@gmail.com>
2025-05-04 07:21:30 -0500

    add changes

--- frontend/src/components/types/technicalService.ts
+++ frontend/src/components/types/technicalService.ts
@@ -1,13 +1,13 @@
-export interface TechnicalServiceEntry {
-  id: string;
+export type TechnicalServiceEntry = {
+  id?: string; // Optional for new devices, required for existing ones
   client: string;
   device: string;
   models: string;
-  IMEI: number;
-  status: string;
+  IMEI: string;
+  status: "En reparación" | "Reparado" | "No reparado";
   entryDate: string;
   exitDate: string | null;
   warrantLimit: string | null;
   price: number;
-  detail: string | null;
-}
+  detail: string;
+};
