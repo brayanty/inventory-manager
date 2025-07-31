e06792c733bedfa59d79d766dbf9860d4a8efc81
brayanty <danielsegoviad@gmail.com>
2025-07-30 23:51:03 -0500

    fix: mejorar la legibilidad del código en el componente Header

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -4,13 +4,17 @@ import { useSearchStore } from "../store/filters.tsx";
 import { useOpenSidebar } from "../hooks/openSidebar.tsx";
 
 function Header() {
-  const { categorySelect, setCategorySelect, categoryList } = useCategoryListStore();
+  const { categorySelect, setCategorySelect, categoryList } =
+    useCategoryListStore();
   const { search, setSearch } = useSearchStore();
-  const [toggleSidebar] = useOpenSidebar("#navegation")
+  const [toggleSidebar] = useOpenSidebar("#navegation");
   return (
     <header className="mx-auto w-full h-full max-h-16 flex justify-between items-center p-5 dark:text-white bg-white dark:bg-[#1f232b]">
       <div>
-        <button className="cursor-pointer" onClick={toggleSidebar}>
+        <button
+          className="cursor-pointer p-2 rounded-md border border-gray-300 dark:border-gray-600 dark:bg-[#2b2f38] hover:bg-gray-600 transition-colors ease-in-out duration-500 text-sm "
+          onClick={toggleSidebar}
+        >
           <Menu />
         </button>
       </div>
