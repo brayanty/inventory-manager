7c875ff2cd976ab0610c1f09d36f5d31cd29c453
brayanty <danielsegoviad@gmail.com>
2025-08-06 14:34:03 -0500

    change: renamed import componet Product to ProductsInventory

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import Product from "@/components/inventory";
+import ProductsInventory from "@/pages/inventory";
 import Layout from "../components/layout/Layout";
 import TechnicalService from "../pages/technicalService";
 import { Route, Routes } from "react-router";
@@ -7,7 +7,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<ProductsInventory />} />
         <Route path="/technicalservice" element={<TechnicalService />} />
       </Route>
     </Routes>
