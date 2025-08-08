51a10e310f07cacd8e8abcc4dbde4b0c1ce95337
brayanty <daniesegoviad@gmail.com>
2025-08-08 14:33:48 -0500

    fix: update product validation logic and response status in product creation endpoint feat: initialize form data on form render based on provided fields refactor: add parseLAPrice utility function for price formatting

--- frontend/src/components/utils/ParsePrice.tsx
+++ frontend/src/components/utils/ParsePrice.tsx
@@ -1,3 +1,4 @@
+//esta funcion convierte un string de precio en formato latinoamericano a un numero flotante
 export const parseLAPrice = (value: string): number => {
    return parseFloat(value.replace(/[$.]/g, "").replace(",", "."));
 }
