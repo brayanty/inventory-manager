352fc59a7c8cadfbb52cd50c766b4ad05075ad28
brayanty <daniesegoviad@gmail.com>
2025-08-24 13:20:41 -0500

    fix: mejoras en la escritura

--- backend/utils/file.js
+++ backend/utils/file.js
@@ -1,4 +1,5 @@
-import fs from  "fs/promises" 
+import fs from "fs/promises";
+import path from "path";
 // Funciones de archivo
 export async function readData(file) {
   try {
@@ -10,17 +11,20 @@ export async function readData(file) {
 }
 
 export async function writeData(newData, file) {
-   const newDataFile = await readData(file);
-    newDataFile.push(newData);
+  const filePath = path.resolve(file);
+
+  let newDataFile = (await readData(filePath)) ?? [];
+  if (!Array.isArray(newDataFile)) {
+    throw new Error("El archivo no contiene un array válido");
+  }
+
+  newDataFile.push(newData);
 
   try {
-    await fs.writeFile(
-      file,
-      JSON.stringify(newDataFile, null, 2)
-    );
-    console.log("Datos escritos correctamente");
+    await fs.writeFile(filePath, JSON.stringify(newDataFile, null, 2), "utf-8");
+    console.log("✅ Datos escritos correctamente");
   } catch (err) {
-    console.error("Error al escribir datos:", err);
+    console.error("❌ Error al escribir datos:", err);
     throw err;
   }
 }
