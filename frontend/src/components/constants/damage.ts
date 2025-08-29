d41efa97a70943b4425f083547f3ea5c02ff2fc5
brayanty <daniesegoviad@gmail.com>
2025-08-28 23:31:13 -0500

    add: damages to devices

---
+++ frontend/src/components/constants/damage.ts
@@ -0,0 +1,86 @@
+export const damages = [
+  {
+    category: "Móvil",
+    repair: [
+      {
+        name: "Cambio de pantalla",
+        time: "1-2 horas",
+      },
+      {
+        name: "Reemplazo de batería",
+        time: "30-60 minutos",
+      },
+      {
+        name: "Reparación de conector de carga",
+        time: "1-2 horas",
+      },
+      {
+        name: "Problemas de software",
+        time: "1-3 horas",
+      },
+    ],
+  },
+  {
+    category: "PC de escritorio",
+    repair: [
+      {
+        name: "Cambio de fuente de poder",
+        time: "1-2 horas",
+      },
+      {
+        name: "Actualización de RAM",
+        time: "30-60 minutos",
+      },
+      {
+        name: "Instalación de sistema operativo",
+        time: "1-2 horas",
+      },
+      {
+        name: "Reparación de placa base",
+        time: "2-4 horas",
+      },
+    ],
+  },
+  {
+    category: "Portátil",
+    repair: [
+      {
+        name: "Reemplazo de teclado",
+        time: "1-2 horas",
+      },
+      {
+        name: "Cambio de pantalla",
+        time: "1-2 horas",
+      },
+      {
+        name: "Reparación de bisagras",
+        time: "2-3 horas",
+      },
+      {
+        name: "Reemplazo de disco duro/SSD",
+        time: "1-2 horas",
+      },
+    ],
+  },
+  {
+    category: "Dispositivo de sonido",
+    repair: [
+      {
+        name: "Reparación de conector de audio",
+        time: "1-2 horas",
+      },
+      {
+        name: "Cambio de altavoz",
+        time: "1-2 horas",
+      },
+      {
+        name: "Reparación de bluetooth",
+        time: "2-3 horas",
+      },
+      {
+        name: "Problemas de batería en altavoces inalámbricos",
+        time: "1-2 horas",
+      },
+    ],
+  },
+];
