1b0ae72c6b427fb983679342b5e801f13b42c732
brayanty <daniesegoviad@gmail.com>
2025-09-02 00:59:22 -0500

    fix: improve on the keys for shoppingCart

--- frontend/src/components/common/shoppingCart.tsx
+++ frontend/src/components/common/shoppingCart.tsx
@@ -4,6 +4,7 @@ import { formatCOP } from "@/components/utils/format";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { Product } from "../types/product";
+import { soldProducts } from "../services/products";
 
 const ShoppingCart = () => {
   const { productsCart, clearProductCart } = useShoppingCartStore();
@@ -21,11 +22,11 @@ const ShoppingCart = () => {
     }
   }, [productsCart]);
 
-  if (!productsCart) return toast("Hubo un error aqui");
+  if (!productsCart) return toast("No hay productos");
 
-  const renderProductsSale = (product: Product) => {
+  const renderProductsSale = (product: Product, index) => {
     return (
-      <li key={product.id} className="flex justify-evenly items-center gap-2">
+      <li key={index} className="flex justify-evenly items-center gap-2">
         <div className="text-[14px]">{product.name.slice(0, 20)}...</div>
         <span className="text-[14px]">{product.category.slice(0, 5)}</span>
         <span className="text-[14px]">{formatCOP(product.price)}</span>
@@ -33,17 +34,21 @@ const ShoppingCart = () => {
     );
   };
 
+  // Función para manejar la venta de productos
+  // Se envia los id de productos vendidos al backend
   const saleProducts = () => {
-    if (productsCart.length === 0) {
-      toast("No hay productos en el carrito");
-      return;
+    if (productsCart.length === 0) toast("No hay productos en el carrito");
+    console.log(productsCart);
+    try {
+      const soldProductId = productsCart.map((item) => item.id);
+      soldProducts(soldProductId);
+      toast("Productos vendidos exitosamente");
+      clearProductCart();
+    } catch {
+      toast("Oooh no, se cayo el server");
     }
-
-    toast("Productos vendidos exitosamente");
-    clearProductCart();
   };
 
-  if (!productsCart) return toast("No hay productos");
   return (
     <div className="grid grid-cols-1 grid-rows-12 gap-1 relative  text-[#7e8590] h-full max-h-full max-w-full bg-[rgba(36,40,50,1)] shadow-[0px_0px_15px_rgba(0,0,0,0.09)]">
       <div className="sticky top-0 row-span-2 p-2 text-xs text-gray-700 bg-gray-400 dark:bg-[rgb(62,67,80)] dark:text-gray-300">
@@ -57,7 +62,11 @@ const ShoppingCart = () => {
         </div>
       </div>
       <main className="row-span-8 row-start-3 h-full max-h-full no-scrollbar overflow-y-scroll overflow-hidden">
-        <ul>{productsCart.map((product) => renderProductsSale(product))}</ul>
+        <ul>
+          {productsCart.map((product, index) =>
+            renderProductsSale(product, index)
+          )}
+        </ul>
       </main>
       <footer className="col-start-1 row-start-11 row-span-2 text-gray-700 dark:text-gray-300 bg-gray-400 dark:bg-[rgb(62,67,80)] p-2 flex items-center justify-between">
         <div>
