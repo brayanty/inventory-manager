947e2a9d3cefa0ff3b37fc7db391600e4ce8ec5d
brayanty <daniesegoviad@gmail.com>
2025-09-07 14:53:15 -0500

    add: separate implemetation for renderProducts

---
+++ frontend/src/components/inventory/hooks/useHandleController.tsx
@@ -0,0 +1,22 @@
+import { Product, ProductsCart } from "@/components/types/product";
+import { toast } from "react-toastify";
+
+export const useHandleController = (
+  productsCart: ProductsCart[],
+  addProductShopping: (product: ProductsCart) => void
+) => {
+  const handleAddShoppingCart = (product: Product) => {
+    const indexSearch = productsCart.findIndex(
+      (productCart) => productCart.id === product.id
+    );
+
+    if (indexSearch === -1) {
+      addProductShopping(product);
+      toast.success("Se agrego correctamente al carrito");
+      return;
+    } else {
+      toast.warn("Ya existe en el carrito");
+    }
+  };
+  return { handleAddShoppingCart };
+};
