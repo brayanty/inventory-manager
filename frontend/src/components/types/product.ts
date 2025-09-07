5d676f5d9e1092078b17f9fa1d969fdcf6d36847
brayanty <daniesegoviad@gmail.com>
2025-09-07 14:49:26 -0500

    add: new type ProductCart to product interface

--- frontend/src/components/types/product.ts
+++ frontend/src/components/types/product.ts
@@ -7,3 +7,14 @@ export type Product = {
   sales: number;
   id?: string;
 };
+
+export type ProductsCart = {
+  name: string;
+  quantity: number;
+  category: string;
+  total?: number;
+  price: number;
+  sales: number;
+  id?: string;
+  amount?: number;
+};
