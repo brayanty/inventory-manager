72fb32c5adcfc1f581bb8f84dcdd7be8772ce636
brayanty <daniesegoviad@gmail.com>
2025-09-14 01:11:00 -0500

    delete: remove unused category state and store

--- frontend/src/components/store/filters.tsx
+++ frontend/src/components/store/filters.tsx
@@ -9,13 +9,3 @@ export const useSearchStore = create<SearchStoreState>((set) => ({
   search: "",
   setSearch: (newSearch) => set({ search: newSearch }),
 }));
-
-interface CategoryState {
-  category: string;
-  setCategory: (category: string) => void;
-}
-
-export const useCategoryStore = create<CategoryState>((set) => ({
-  category: "",
-  setCategory: (newCategory) => set({ category: newCategory }),
-}));
