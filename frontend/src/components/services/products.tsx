a128ac8f9258e7e6b7e55be4c67f43ef30721f09
brayanty <daniesegoviad@gmail.com>
2025-09-15 23:28:26 -0500

    feat: add createCategory function to handle category creation

--- frontend/src/components/services/products.tsx
+++ frontend/src/components/services/products.tsx
@@ -66,3 +66,15 @@ export async function getCategories() {
   const data = await response.json();
   return data;
 }
+
+export async function createCategory(category) {
+  const response = await fetch(DEVICE_ENDPOINT + "/categories", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(category),
+  });
+  const data = await response.json();
+  return data;
+}
