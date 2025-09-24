053a5ae7ce464d0bd690bb6dae440140545f0759
brayanty <daniesegoviad@gmail.com>
2025-09-24 14:27:01 -0500

    refactor: update ShoppingCart state to use ProductsCart type and enhance addProductShopping logic

--- frontend/src/components/store/ShoppingCart.tsx
+++ frontend/src/components/store/ShoppingCart.tsx
@@ -1,10 +1,10 @@
 import { create } from "zustand";
-import { Product } from "../types/product";
+import { ProductsCart } from "../types/product";
 
 // Definición del estado global
 interface ShoppingCartState {
-  productsCart: Product[];
-  addProductShopping: (product: Product) => void;
+  productsCart: ProductsCart[];
+  addProductShopping: (product: ProductsCart) => void;
   removeProductCart: (id: string) => void;
   clearProductCart: () => void;
 }
@@ -12,8 +12,19 @@ interface ShoppingCartState {
 // Uso de create para crear el store
 const useShoppingCartStore = create<ShoppingCartState>((set) => ({
   productsCart: [],
-  addProductShopping: (product) =>
-    set((state) => ({ productsCart: [...state.productsCart, product] })),
+  addProductShopping: (product: ProductsCart) => {
+    set((state) => {
+      const productExist = state.productsCart.find((p) => p.id === product.id);
+      if (productExist) {
+        const newProductCart = state.productsCart.map((i) => {
+          return i.id === product.id ? product : i;
+        });
+        return { productsCart: newProductCart };
+      }
+
+      return { productsCart: [...state.productsCart, product] };
+    });
+  },
   removeProductCart: (id) =>
     set((state) => ({
       productsCart: state.productsCart.filter((product) => product.id !== id),
