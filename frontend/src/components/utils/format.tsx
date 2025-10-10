834a57e8c0c586de5faced6e69b0f81f1d39c9c2
brayanty <danielsegoviad@gmail.com>
2025-10-09 22:57:52 -0500

    feat: remove componet PriceInput for fomat file

--- frontend/src/components/utils/format.tsx
+++ frontend/src/components/utils/format.tsx
@@ -1,28 +1,7 @@
-import { NumericFormat }from "react-number-format";
+import { NumericFormat } from "react-number-format";
 
 export const formatCOP = (valor: number) =>
   new Intl.NumberFormat("es-CO", {
     style: "currency",
     currency: "COP",
   }).format(valor);
-
-
-
-export  const PriceInput = ({ value, onChange }: {value: number,onChange: (value: number)=> void}) => {
-  return (
-    <label className="input input-bordered flex items-center gap-2 w-full max-w-xs">
-      $
-      <NumericFormat
-        value={value}
-        thousandSeparator="."
-        decimalSeparator=","
-        decimalScale={2}
-        fixedDecimalScale
-        allowNegative={false}
-        onValueChange={(values) => onChange(values.floatValue || 0)}
-        className="grow bg-transparent outline-none"
-        placeholder="Precio"
-      />
-    </label>
-  );
-};
