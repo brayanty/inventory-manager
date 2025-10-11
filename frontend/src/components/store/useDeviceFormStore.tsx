7589fc1ab75fcc4914f380481f195a5967a05f51
brayanty <danielsegoviad@gmail.com>
2025-10-10 21:40:25 -0500

    feat: add useDeviceFormStore for managing device form state

---
+++ frontend/src/components/store/useDeviceFormStore.tsx
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { DeviceEntry } from "../types/technicalService";
+
+interface DeviceFormState {
+  deviceForm: DeviceEntry;
+  setPriceForm: (price: number) => void;
+  setDeviceForm: (name: string, value: string | number) => void;
+  setDeviceFormEdit: (editDevice: DeviceEntry) => void;
+}
+
+export const useDeviceFormStore = create<DeviceFormState>((set) => ({
+  deviceForm: {
+    client: "",
+    device: "",
+    damage: "",
+    model: "",
+    IMEI: "",
+    price: 0,
+    detail: "",
+    faults: [],
+    pay: false,
+  },
+  setPriceForm: (price) =>
+    set((state) => ({
+      deviceForm: {
+        ...state.deviceForm,
+        price,
+      },
+    })),
+  setDeviceForm: (name, value) =>
+    set((state) => ({
+      deviceForm: {
+        ...state.deviceForm,
+        [name]: value,
+      },
+    })),
+  setDeviceFormEdit: (editDevice) => set({ deviceForm: editDevice }),
+}));
