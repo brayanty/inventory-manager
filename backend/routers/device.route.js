59a72cbb61fdc66828dff7cfbd084fe51d017b37
brayanty <danielsegoviad@gmail.com>
2025-10-18 14:40:52 -0500

    refactor(backend): update imports

--- backend/routers/device.route.js
+++ backend/routers/device.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import createDevice from "../controllers/createDevice.js";
-import getAllDevice from "../controllers/getAllDevice.js";
-import getDevice from "../controllers/getDevice.js";
-import updateDevice from "../controllers/updateDevice.js";
-import deleteDevice from "../controllers/deleteDevice.js";
-import getRepairs from "../controllers/getRepairs.js";
+import createDevice from "../controllers/deviceRepair/createDevice.js";
+import getAllDevice from "../controllers/deviceRepair/getAllDevice.js";
+import getDevice from "../controllers/deviceRepair/getDevice.js";
+import updateDevice from "../controllers/deviceRepair/updateDevice.js";
+import deleteDevice from "../controllers/deviceRepair/deleteDevice.js";
+import getRepairs from "../controllers/deviceRepair/getRepairs.js";
 
 const router = Router();
 
