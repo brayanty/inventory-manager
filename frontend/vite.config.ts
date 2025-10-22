36025690614352b56e8b36cb828cc8ae3373b5ba
brayanty <danielsegoviad@gmail.com>
2025-10-22 01:01:47 -0500

    refactor(frontend): configure HTTPS settings and update server port in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { readFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,11 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    https: {
+      key: readFileSync(path.resolve(__dirname, "ssl/key.pem")),
+      cert: readFileSync(path.resolve(__dirname, "ssl/cert.pem")),
+    },
+    port: 5173,
     host: "0.0.0.0",
   },
 });
