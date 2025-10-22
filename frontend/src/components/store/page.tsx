30ff285a3b187646db24f264034ed1159c496dde
brayanty <danielsegoviad@gmail.com>
2025-10-21 22:05:36 -0500

    refactor(frontend): new properties for usePageStore

--- frontend/src/components/store/page.tsx
+++ frontend/src/components/store/page.tsx
@@ -3,11 +3,15 @@ import { create } from "zustand";
 interface PageStoreState {
   page: number;
   setPage: (page: number) => void;
+  totalPages: number;
+  setTotalPages: (page: number) => void;
 }
 
 const usePageStore = create<PageStoreState>((set) => ({
   page: 1,
   setPage: (page) => set({ page: page }),
+  totalPages: 0,
+  setTotalPages: (totalPages) => set({ totalPages: totalPages }),
 }));
 
 export default usePageStore;
