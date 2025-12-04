ae1427f61ab7af2664a9c9f8bc16e24d9f05e0a1
brayanty <danielsegoviad@gmail.com>
2025-12-04 00:15:54 -0500

    feat(backend): add route for retrieving sold products

--- backend/routers/product.route.js
+++ backend/routers/product.route.js
@@ -4,6 +4,7 @@ import { createProduct } from "../controllers/product/createProduct.js";
 import { updateProduct } from "../controllers/product/updateProduct.js";
 import { soldProduct } from "../controllers/product/soldProduct.js";
 import { deleteProduct } from "../controllers/product/deleteProduct.js";
+import { getSoldProduct } from "../controllers/product/getSoldProduct.js";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.post("/products", createProduct);
 router.put("/products/:id", updateProduct);
 router.delete("/products/:id", deleteProduct);
 router.post("/soldProducts", soldProduct);
+router.get("/soldProducts", getSoldProduct);
 
 export default router;
