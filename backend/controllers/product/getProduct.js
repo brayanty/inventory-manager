81de6e493a75a0f18275e8e7e70c94ae65b5e6d8
brayanty <danielsegoviad@gmail.com>
2025-12-08 13:59:24 -0500

    feat(backend): refactor getProduct function to use database queries for product retrieval

--- backend/controllers/product/getProduct.js
+++ backend/controllers/product/getProduct.js
@@ -1,42 +1,52 @@
-import Fuse from "fuse.js";
-import { FILES } from "../../config/file.js";
-import { readData } from "../../utils/file.js";
+import pool from "../../config/db.js";
 import { handleError, handleSuccess } from "../../modules/handleResponse.js";
 
-// Obtener productos con paginación y búsqueda
 export async function getProduct(req, res) {
   const { search, page = 1, limit = 10 } = req.query;
-  try {
-    const entries = await readData(FILES.PRODUCTS);
 
-    let filtered = entries;
-    // Filtrar si hay búsqueda
+  const pageNum = parseInt(page) || 1;
+  const limitNum = parseInt(limit) || 10;
+  const offset = (pageNum - 1) * limitNum;
+
+  try {
+    let where = "";
+    let params = [];
+
     if (search) {
-      const fuse = new Fuse(entries, {
-        keys: ["name"],
-        includeScore: true,
-        threshold: 0.3,
-      });
-      filtered = fuse.search(search).map((r) => r.item);
+      where = "WHERE name ILIKE $1";
+      params.push(`%${search}%`);
     }
 
-    // Calcular paginado
-    const pageNum = parseInt(page) || 1;
-    const limitNum = parseInt(limit) || 10;
-    const totalItems = filtered.length;
-    const totalPages = Math.ceil(totalItems / limitNum);
-    const start = (pageNum - 1) * limitNum;
-    const paginated = filtered.slice(start, start + limitNum);
+    const productsQuery = `
+      SELECT *
+      FROM products
+      ${where}
+      ORDER BY id DESC
+      LIMIT ${limitNum} OFFSET ${offset};
+    `;
 
-    handleSuccess(req, res, {
+    const products = await pool.query(productsQuery, params);
+
+    const countQuery = `
+      SELECT COUNT(*) AS total
+      FROM products
+      ${where};
+    `;
+
+    const total = await pool.query(countQuery, params);
+
+    const totalItems = parseInt(total.rows[0].total);
+    const totalPages = Math.ceil(totalItems / limitNum);
+
+    return handleSuccess(req, res, {
       page: pageNum,
       limit: limitNum,
       totalItems,
       totalPages,
-      products: paginated,
+      products: products.rows,
     });
   } catch (error) {
     console.error(error);
-    handleError(req, res, "Error al leer los productos");
+    return handleError(req, res, "Error al leer los productos");
   }
 }
