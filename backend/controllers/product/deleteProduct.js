af450be22660114b64bf6add73212f3361e9549c
brayanty <danielsegoviad@gmail.com>
2026-01-26 09:00:21 -0500

    refactor(backend): add product deleted to database

--- backend/controllers/product/deleteProduct.js
+++ backend/controllers/product/deleteProduct.js
@@ -1,16 +1,25 @@
-import { FILES } from "../../config/file.js";
+import pool from "../../config/db.js";
 import { handleError, handleSuccess } from "../../modules/handleResponse.js";
-import { overwriteData, readData } from "../../utils/file.js";
 
 export async function deleteProduct(req, res) {
   const productID = req.params.id;
 
   try {
-    const products = await readData(FILES.PRODUCTS);
-    const newProducts = products.filter((p) => p.id !== productID);
-    await overwriteData(newProducts, FILES.PRODUCTS);
-    handleSuccess(req, res, productID);
-  } catch {
-    handleError(req, res, "Error al eliminar el producto");
+    if (productID == null)
+      return handleError(req, res, "El id esta vacío o inexistente");
+
+    const result = await pool.query(
+      "UPDATE products SET deleted_at = NOW() WHERE id = $1 AND deleted_at IS NULL RETURNING *",
+      [productID],
+    );
+
+    if (result.rowCount === 0) {
+      return handleError(req, res, "Producto no encontrado o ya borrado", 404);
+    }
+
+    handleSuccess(req, res, result.rows[0], "Producto borrado");
+  } catch (error) {
+    console.error(error);
+    return handleError(req, res, "Error al eliminar el producto");
   }
 }
