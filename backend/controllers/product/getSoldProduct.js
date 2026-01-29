42502e9f6522738cf04d1f7faf112da45dd10cf6
brayanty <danielsegoviad@gmail.com>
2026-01-29 15:42:42 -0500

    refactor(backend): implement database connection in getSoldProduct

--- backend/controllers/product/getSoldProduct.js
+++ backend/controllers/product/getSoldProduct.js
@@ -1,37 +1,70 @@
-import Fuse from "fuse.js";
-import { FILES } from "../../config/file.js";
-import { readData } from "../../utils/file.js";
-import { searchCoincide } from "../../utils/searchCoincide.js";
 import { handleError, handleSuccess } from "../../modules/handleResponse.js";
+import pool from "../../config/db.js";
+import { testDate } from "../../utils/validateDate.js";
 
 // Obtener productos con paginación y búsqueda
 export async function getSoldProduct(req, res) {
-  const { page = 1, limit = 10, date } = req.query;
+  const { page = 1, limit = 10, date1, date2 } = req.query;
 
-  const search = new Date(date).toISOString().split("T")[0];
-  console.log("Search Date:", search);
+  // Valida las fechas
+  if (!testDate.test(date1) || !testDate.test(date2)) {
+    return handleError(req, res, "Las fechas no son correctas", 409);
+  }
+  // Valida que limit no se un numero mayor a 100
+  if (isNaN(page) || isNaN(limit) || limit > 100 || page < 1)
+    return handleError(
+      req,
+      res,
+      "El numero de pagina no es valido o limite excede el valor de 100",
+      404,
+      `page = ${page} limit = ${limit}`,
+    );
+
+  const pageNum = parseInt(page);
+  const limitNum = parseInt(limit);
+  const offset = (pageNum - 1) * limitNum;
+
+  let where = `WHERE sold_at >= $1::date AND sold_at < $2::date + INTERVAL '1 day'`;
+  let params = [date1, date2];
 
   try {
-    const soldProduct = await readData(FILES.SALES);
+    // Total de registros
+    const countQuery = `
+      SELECT COUNT(*) 
+      FROM soldProduct
+      ${where}
+    `;
 
-    const filtered = searchCoincide(soldProduct, search, ["date"]);
-    // Calcular paginado
-    const pageNum = parseInt(page) || 1;
-    const limitNum = parseInt(limit) || 10;
-    const totalItems = filtered.length;
+    const totalResult = await pool.query(countQuery, params);
+    const totalItems = parseInt(totalResult.rows[0].count);
     const totalPages = Math.ceil(totalItems / limitNum);
-    const start = (pageNum - 1) * limitNum;
-    const paginated = filtered.slice(start, start + limitNum);
 
-    handleSuccess(req, res, {
+    // Datos paginados
+    const dataQuery = `
+      SELECT *
+      FROM soldProduct
+      ${where}
+      ORDER BY sold_at DESC
+      LIMIT $${params.length + 1}
+      OFFSET $${params.length + 2}
+    `;
+
+    const dataParams = [...params, limitNum, offset];
+    const soldProductQuery = await pool.query(dataQuery, dataParams);
+
+    if (soldProductQuery.rowCount == 0) {
+      return handleError(req, res, "No se encontraron datos de venta", 404);
+    }
+
+    return handleSuccess(req, res, {
       page: pageNum,
       limit: limitNum,
       totalItems,
       totalPages,
-      soldProduct: paginated,
+      soldProduct: soldProductQuery.rows,
     });
   } catch (error) {
-    console.error(error);
-    handleError(req, res, "Error al leer los productos");
+    console.log(error);
+    return handleError(req, res, "Error del servidor", 500);
   }
 }
