03b805efbbd08b8de0e93ae775dbe90a7c2e23f6
brayanty <danielsegoviad@gmail.com>
2026-02-08 11:53:53 -0500

    refactor(backend): implement database connection in deleteDevice

--- backend/controllers/deviceRepair/deleteDevice.js
+++ backend/controllers/deviceRepair/deleteDevice.js
@@ -1,29 +1,33 @@
-import { FILES } from "../../config/file.js";
+import pool from "../../config/db.js";
 import { handleError, handleSuccess } from "../../modules/handleResponse.js";
-import { overwriteData, readData } from "../../utils/file.js";
 
 export default async function deleteDevice(req, res) {
   const deviceID = req.params.id;
+  if (isNaN(deviceID) && !deviceID) {
+    return handleError(req, res, "No se proporciono un id valido");
+  }
+  const client = await pool.connect();
   try {
-    const devices = await readData(FILES.DEVICES);
+    await client.query("BEGIN");
 
-    const deviceExists = devices.some((e) => e.id === deviceID);
-
-    if (!deviceExists) {
-      return handleError(
-        req,
-        res,
-        "El dispositivo no existe el la base de datos",
-        404
-      );
+    const { rows } = await client.query(
+      "UPDATE device SET deleted_at = NOW() WHERE id = $1 RETURNING*",
+      [deviceID],
+    );
+    if (rows.length != 1) {
+      throw new Error("ROLLBACK");
     }
 
-    const newdevices = devices.filter((e) => e.id !== deviceID);
-
-    await overwriteData(newdevices, FILES.DEVICES);
-
+    await client.query("COMMIT");
     return handleSuccess(req, res, deviceID, "Dispositivo eliminado");
-  } catch {
+  } catch (err) {
+    console.log(err);
+    if (err.messege == "ROLLBACK") {
+      await client.query("ROLLBACK");
+      handleError(req, res, "El dispositivo no existe");
+    }
     return handleError(req, res, "Error al eliminar el dispositivo");
+  } finally {
+    client.release();
   }
 }
