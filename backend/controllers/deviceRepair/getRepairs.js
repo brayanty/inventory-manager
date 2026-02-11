1e665d6ab3798df3373455f1d96fb573ff06f7c0
brayanty <danielsegoviad@gmail.com>
2026-02-10 22:22:59 -0500

    refactor(backend): implement database in getRepairs

--- backend/controllers/deviceRepair/getRepairs.js
+++ backend/controllers/deviceRepair/getRepairs.js
@@ -1,47 +1,36 @@
-import Fuse from "fuse.js";
-import { FILES } from "../../config/file.js";
 import { handleError, handleSuccess } from "../../modules/handleResponse.js";
-import { readData } from "../../utils/file.js";
+import pool from "../../config/db.js";
+
+const repairCategoryAvailable = ["repuesto", "display", "servicio"];
 
 export default async function getRepairs(req, res) {
   const search = req.query.search;
+  if (!search) {
+    return handleError(req, res, "El parámetro de búsqueda está vacío", 404);
+  }
+
+  const client = await pool.connect();
   try {
-    if (!search) {
-      return handleError(req, res, "El parámetro de búsqueda está vacío", 404);
-    }
+    const queryRepairs =
+      "SELECT * FROM product WHERE category = $1 OR category = $2 OR category = $3 AND name ILIKE $4 AND deleted_at IS NULL AND stock >= 1 ";
 
-    const repairTypeAvailable = await readData(FILES.PRODUCTS);
-
-    const filterRepairType = repairTypeAvailable.filter((repair) => {
-      return (
-        repair.category == "repuesto" ||
-        repair.category == "display" ||
-        repair.category == "servicio"
-      );
-    });
-    const fuse = new Fuse(filterRepairType, {
-      keys: ["name"],
-      includeScore: true,
-      threshold: 0.3,
-    });
-
-    const results = fuse.search(search);
-
-    if (!results.length) {
+    const { rows, rowCount } = await client.query(queryRepairs, [
+      ...repairCategoryAvailable,
+      search,
+    ]);
+    console.log(rows);
+    if (rowCount <= 0) {
       return handleError(
         req,
         res,
         "No se encontraron resultados para la búsqueda",
-        404
+        404,
       );
     }
-    handleSuccess(
-      req,
-      res,
-      results.map((r) => r.item)
-    );
-  } catch (error) {
-    console.error("Error al leer las reparaciones disponibles:", error);
+
+    handleSuccess(req, res, rows);
+  } catch (err) {
+    console.error("Error al leer las reparaciones disponibles:", err);
     handleError(req, res, "Error al leer las reparaciones disponibles", 500);
   }
 }
