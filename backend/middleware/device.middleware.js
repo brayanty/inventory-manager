2d2722a44916f96c127493d0d514a866481ed053
brayanty <danielsegoviad@gmail.com>
2026-02-21 23:47:41 -0500

    refactor(backend): renamed file createDevice.middleware.js to device.middleware.js

--- backend/middleware/device.middleware.js
+++ backend/middleware/device.middleware.js
@@ -1,11 +1,15 @@
-import { deviceSchema } from "../validators/createDevice.schema.js";
+import {
+  deviceSchema,
+  deviceUpdateSchema,
+} from "../validators/createDevice.schema.js";
 import { handleError } from "../modules/handleResponse.js";
 
 export function validateDevice(req, res, next) {
   const result = deviceSchema.safeParse(req.body);
 
   if (!result.success) {
-    return handleError(req, res, "No hay se recibieron datos validos", 404);
+    handleError(req, res, result.error.message, 400);
+    return;
   }
 
   req.body = result.data;
