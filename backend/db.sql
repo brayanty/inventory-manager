808b211274e29cd947ff640e2cc7c6a787eb99ba
brayanty <danielsegoviad@gmail.com>
2026-02-24 14:41:40 -0500

    feat(database): enhance schema with new tables and triggers for product and device management

--- backend/db.sql
+++ backend/db.sql
@@ -1,69 +1,115 @@
-CREATE TABLE product (             
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  category VARCHAR(100),
-  stock INT NOT NULL CHECK (stock >= 0),
-  price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
-  deleted_at TIMESTAMP WITH TIME ZONE
+CREATE TABLE IF NOT EXISTS product (
+      id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      name VARCHAR(250) NOT NULL,
+      category VARCHAR(100),
+      stock INT NOT NULL CHECK (stock >= 0),
+      price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+      deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-INSERT INTO product (name,category,price,stock) VALUES('nosenn','display',10000,100);
-
--- DROP TABLE IF EXISTS product;
-
-CREATE TABLE soldProduct (  
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  clientName VARCHAR(250),
-  sales INT NOT NULL CHECK (sales >= 0),
-  sold_at TIMESTAMP WITH TIME ZONE,
-  price INT NOT NULL,
-  productID INT NOT NULL,
-  CONSTRAINT fk_product
-    FOREIGN KEY (productID)
-    REFERENCES product(id)
+CREATE TABLE IF NOT EXISTS soldProduct (
+      id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      client_name VARCHAR(250),
+      sales INT NOT NULL CHECK (sales > 0),
+      category VARCHAR(250) NOT NULL,
+      sold_at TIMESTAMP WITH TIME ZONE,
+      price NUMERIC(10, 2) NOT NULL,
+      product_id INT NOT NULL,
+      CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
 );
-
-
-
-DROP TABLE IF EXISTS soldProduct;
-
-INSERT INTO soldProduct (sale,price,productID) VALUES(10000,500,100);
-
-
-
- SELECT * FROM product;
-
-
 
 CREATE INDEX idx_soldproduct_sold_at ON soldProduct (sold_at);
-CREATE INDEX idx_soldproduct_productid ON soldProduct (productID);
+
+CREATE INDEX idx_soldproduct_productid ON soldProduct (product_id);
+
 CREATE INDEX idx_product_deleted_at ON product (deleted_at);
 
+-- Tableas device
+CREATE TABLE IF NOT EXISTS device (
+      id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      client_name VARCHAR(250),
+      number_phone VARCHAR(20) CHECK (number_phone ~ '^\+?[0-9]{7,15}$'),
+      device VARCHAR(250),
+      model VARCHAR(100),
+      price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+      price_pay NUMERIC(10, 2) CHECK (price_pay >= 0),
+      detail VARCHAR(400),
+      faults JSONB,
+      repair_status VARCHAR(25) NOT NULL DEFAULT 'En Revisión' CHECK (
+            repair_status IN (
+                  'Reparado',
+                  'Sin Solución',
+                  'En Revisión'
+            )
+      ),
+      pay BOOLEAN NOT NULL DEFAULT false,
+      output_status BOOLEAN NOT NULL DEFAULT false,
+      imei VARCHAR(15) CHECK (length(imei) = 15),
+      entry_date TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+      exit_date TIMESTAMP WITH TIME ZONE,
+      warrant_limit TIMESTAMP WITH TIME ZONE,
+      deleted_at TIMESTAMP WITH TIME ZONE
+);
 
--- Tableas
+-- 1. Primero creamos o reemplazamos la función
+CREATE
+OR REPLACE FUNCTION set_pay_status() RETURNS TRIGGER AS $ $ BEGIN -- Comparamos si el estado es Reparado y el pago es completo
+IF NEW.repair_status = 'Reparado'
+AND NEW.price_pay IS NOT NULL
+AND NEW.price_pay >= NEW.price THEN NEW.pay := true;
 
-CREATE TABLE IF NOT EXISTS device(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    client_name VARCHAR(250),
-    number_phone VARCHAR(20) CHECK (number_phone ~ '^\+?[0-9]{7,15}$'),
-    device VARCHAR(250),
-    model VARCHAR(100),
-    price INT NOT NULL,
-    detail VARCHAR(400),
-    faults JSONB NOT NULL,
-    repair_status VARCHAR(25) CHECK (repair_status IN ('Reparado', 'Sin Solución', 'En Revisión')) NOT NULL,
-    pay boolean DEFAULT FALSE,
-    output_status VARCHAR(5),
-    price_pay INT NOT NULL,
-    imei VARCHAR(15) CHECK (length(imei) = 15),
-    entry_date  TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    exit_date TIMESTAMP WITH TIME ZONE,
-    warrant_limit TIMESTAMP WITH TIME ZONE,
-    deleted_at TIMESTAMP WITH TIME ZONE
-)
+ELSE NEW.pay := false;
 
+END IF;
 
-ALTER TABLE device
-ADD COLUMN IF NOT EXISTS TIMESTAMP WITH TIME ZONE;
-SELECT * FROM device
-DROP TABLE device
+RETURN NEW;
+
+END;
+
+$ $ LANGUAGE plpgsql;
+
+-- 2. Creamos el TRIGGER (Esto es lo que hace que la función se ejecute)
+-- Se ejecutará automáticamente antes de cada INSERT o UPDATE
+CREATE
+OR REPLACE TRIGGER trigger_device_pay BEFORE
+INSERT
+      OR
+UPDATE
+      ON device FOR EACH ROW EXECUTE FUNCTION set_pay_status();
+
+-- 1. Primero creamos o reemplazamos la función
+CREATE
+OR REPLACE FUNCTION set_pay_status() RETURNS TRIGGER AS $ $ BEGIN -- Comparamos si el estado es Reparado y el pago es completo
+IF NEW.repair_status = 'Reparado'
+AND NEW.price_pay IS NOT NULL
+AND NEW.price_pay >= NEW.price THEN NEW.pay := true;
+
+ELSE NEW.pay := false;
+
+END IF;
+
+RETURN NEW;
+
+END;
+
+$ $ LANGUAGE plpgsql;
+
+-- 2. Creamos el TRIGGER (Esto es lo que hace que la función se ejecute)
+-- Se ejecutará automáticamente antes de cada INSERT o UPDATE
+CREATE
+OR REPLACE TRIGGER trigger_device_pay BEFORE
+INSERT
+      OR
+UPDATE
+      ON device FOR EACH ROW EXECUTE FUNCTION set_pay_status();
+
+CREATE TABLE IF NOT EXISTS historyTicket (
+      id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      device_id INT NOT NULL,
+      total_price NUMERIC(10, 2) NOT NULL,
+      spare_parts JSONB NOT NULL,
+      timestamp TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+      CONSTRAINT fk_device FOREIGN KEY (device_id) REFERENCES device(id)
+);
+
+CREATE INDEX idx_history_spare_parts ON historyTicket USING GIN (spare_parts);
