3ec75adb07c6febe488381decc3889046f8a4317
brayanty <danielsegoviad@gmail.com>
2026-03-04 00:14:05 -0500

    fix(frontend): correct mapping of faults in device form submission

--- frontend/src/pages/technicalService.tsx
+++ frontend/src/pages/technicalService.tsx
@@ -66,7 +66,7 @@ const TechnicalService = () => {
     const newPrice = Number(deviceForm.price);
     const newPricePay = Number(deviceForm.price_pay);
     const newNumberPhone = deviceForm.number_phone.split("-").join("");
-    const newFaults = deviceForm.faults.map((f)=>(f.id))
+    const newFaults = deviceForm.faults.map((f)=>({id:f.id}))
     e.preventDefault();
     if (
       !deviceForm.client_name.trim() ||
