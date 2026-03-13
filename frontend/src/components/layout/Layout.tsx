9f5abb0607225900e3532e6aab792d271ce9bde5
brayanty <danielsegoviad@gmail.com>
2026-03-12 23:27:19 -0500

    delete(frontend): remove ShoppingCartIcon for Layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "./header";
 import Sidebar from "./Sidebar";
-import { ShoppingCartIcon } from "lucide-react";
 
 export default function Layout() {
   return (
