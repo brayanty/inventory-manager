e135b0546112c5023bf43a5e1730d58c9294c484
brayanty <danielsegoviad@gmail.com>
2026-03-15 21:29:39 -0500

    fix(frontend): remove import tsconfigPaths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
@@ -10,7 +9,6 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
-      tsconfigPaths(),
     ],
 
     build: {
