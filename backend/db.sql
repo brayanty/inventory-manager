60ab6240b58663fdb802bcf550143b531275c637
brayanty <danielsegoviad@gmail.com>
2026-03-29 22:23:42 -0500

    refactor(backend): delete check number_phone and imei

--- backend/db.sql
+++ backend/db.sql
@@ -4,9 +4,7 @@
 CREATE TABLE IF NOT EXISTS category (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(250),
-    deleted_at TIMESTAMP
-    WITH
-        TIME ZONE
+    deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 -- =========================================
@@ -18,9 +16,7 @@ CREATE TABLE IF NOT EXISTS product (
     category VARCHAR(100),
     stock INT NOT NULL CHECK (stock >= 0),
     price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
-    deleted_at TIMESTAMP
-    WITH
-        TIME ZONE
+    deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE INDEX IF NOT EXISTS idx_product_deleted_at ON product (deleted_at);
@@ -33,12 +29,10 @@ CREATE TABLE IF NOT EXISTS soldProduct (
     client_name VARCHAR(250),
     sales INT NOT NULL CHECK (sales > 0),
     category VARCHAR(250) NOT NULL,
-    sold_at TIMESTAMP
-    WITH
-        TIME ZONE,
-        price NUMERIC(10, 2) NOT NULL,
-        product_id INT NOT NULL,
-        CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product (id)
+    sold_at TIMESTAMP WITH TIME ZONE,
+    price NUMERIC(10, 2) NOT NULL,
+    product_id INT NOT NULL,
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_soldproduct_sold_at ON soldProduct (sold_at);
@@ -51,9 +45,7 @@ CREATE INDEX IF NOT EXISTS idx_soldproduct_productid ON soldProduct (product_id)
 CREATE TABLE IF NOT EXISTS device (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     client_name VARCHAR(250),
-    number_phone VARCHAR(20) CHECK (
-        number_phone ~ '^\+?[0-9]{7,15}$'
-    ),
+    number_phone VARCHAR(10),
     device VARCHAR(250),
     model VARCHAR(100),
     price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
@@ -69,27 +61,19 @@ CREATE TABLE IF NOT EXISTS device (
     ),
     pay BOOLEAN NOT NULL DEFAULT false,
     output_status BOOLEAN NOT NULL DEFAULT false,
-    imei VARCHAR(15) CHECK (length(imei) = 15),
-    entry_date TIMESTAMP
-    WITH
-        TIME ZONE DEFAULT NOW(),
-        exit_date TIMESTAMP
-    WITH
-        TIME ZONE,
-        warrant_limit TIMESTAMP
-    WITH
-        TIME ZONE,
-        deleted_at TIMESTAMP
-    WITH
-        TIME ZONE,
-        CONSTRAINT pay_consistency_check CHECK (
-            (
-                (pay = true)
-                AND (price_pay IS NOT NULL)
-                AND (price_pay >= price)
-            )
-            OR (pay = false)
+    imei VARCHAR(15),
+    entry_date TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    exit_date TIMESTAMP WITH TIME ZONE,
+    warrant_limit TIMESTAMP WITH TIME ZONE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT pay_consistency_check CHECK (
+        (
+            (pay = true)
+            AND (price_pay IS NOT NULL)
+            AND (price_pay >= price)
         )
+        OR (pay = false)
+    )
 );
 
 -- =========================================
@@ -128,10 +112,8 @@ CREATE TABLE IF NOT EXISTS historyTicket (
     device_id INT NOT NULL,
     total_price NUMERIC(10, 2) NOT NULL,
     spare_parts JSONB NOT NULL,
-    timestamp TIMESTAMP
-    WITH
-        TIME ZONE DEFAULT NOW(),
-        CONSTRAINT fk_device FOREIGN KEY (device_id) REFERENCES device (id)
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    CONSTRAINT fk_device FOREIGN KEY (device_id) REFERENCES device (id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_history_spare_parts ON historyTicket USING GIN (spare_parts);
