a5b6c417f5606964f229fd95031fe3955cf70573
brayanty <danielsegoviad@gmail.com>
2026-05-05 14:25:00 -0500

    feat(backend): refactor logger configuration for improved readability

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,12 +1,16 @@
 import pino from "pino";
+import loadConfig from "./env";
+
+const isProd = loadConfig().NODE_ENV === "production" ? "info" : "debug";
 
 const logger = pino({
-  transport:
-    process.env.NODE_ENV == "production"
-      ? {
-          target: "pino-pretty",
-          options: { colorize: true },
-        }
-      : undefined,
+    level: isProd,
+    transport: !isProd
+        ? {
+              target: "pino-pretty",
+              options: { colorize: true },
+          }
+        : undefined,
 });
+
 export default logger;
